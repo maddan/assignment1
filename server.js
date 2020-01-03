5a82ef501f3254450ac268366db4ee3f22818d19
jayanti Magar <jayantimagar@jayantis-MacBook-Air-3.local>
2020-01-02 21:02:43 -0500

    Updated to deploy to heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
+
+// PathLocationStrategy
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+console.log('Console listening!');
+
