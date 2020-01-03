8f23774bc0d163a6ff64062599416dd0cf1c7734
jayanti Magar <jayantimagar@jayantis-MacBook-Air-3.local>
2020-01-02 21:49:40 -0500

    Fix app name

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist/angular-calculator1'));
+app.use(express.static(__dirname + '/dist/angular-calculator'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angular-calculator1/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular-calculator/index.html'));
 });
 
 // Start the app by listening on the default
